002565f2727be3efbb1cb727281cfa8a678e9698
Bavaneeswar K <bavaneeswaran49@gmail.com>
2025-08-02 10:32:03 +0530

    Add files via upload
    if ,if else ,else condition statement practice

---
+++ taskjs.js
@@ -0,0 +1,149 @@
+let bavan=1;
+if(bavan>=90) {
+    console.log("AA");
+}
+else if(bavan>=80) {
+     console.log("AB");
+}
+else if(bavan>=70) {
+    console.log("AC");
+}
+else if(bavan>=60) {
+    console.log("AD");
+}else if (bavan>=50){
+    console.log("AE");
+}else if(bavan>=40){
+    console.log("Ai");
+}else if(bavan>=30){
+    console.log("A1");
+}else if (bavan>=0){
+    console.log("aeiou");
+}
+
+let a=8;
+let b=100;
+if(a/b){
+    console.log("even");
+}else if(a/b){
+    console.log("odd");
+}
+let a=8;
+if (a % 2 == 0){
+    console.log("a is a even number");
+}else if(a%2 !=0){
+    console.log("a is an odd number");
+}else(a=!0)
+{
+    console.log("a is a negative integer");
+}
+let age=25;
+if (age>=18)
+{
+    console.log("the candidate is elidgable");
+}
+else if(age<18)
+{
+    console.log("the candidate is unavailablr");
+}
+let bavan = 35;
+if (bavan>=90){
+    console.log("Excellent");
+}else if(bavan>=70){
+    console.log("Good");
+}else if(bavan>=50){
+    console.log("Average");
+}else if (bavan<50)
+{
+    console.log("Fail");
+}
+
+
+
+let age=75;
+if (age<=2){
+    console.log("Infant");
+}
+else if(age<=12){
+    console.log("Child")
+}
+else if(age<=19){
+    console.log("teen");
+}else if(age<=64){
+    console.log("Adult");
+}
+else if(age<=64){
+    console.log("senior");
+}
+else(age<=0){
+    console.log("not born");
+}
+
+let a=-6;
+if(a==0){
+    console.log("Null or Zero");
+}
+else if(a>=1){
+    console.log("Its a Positive Number");
+}
+else if(a<=-1){
+    console.log("Its a Negative number");
+}
+
+let a="d";
+if (a==="a"){
+    console.log("Its a vowels");
+}
+else if (a==="e"){
+    console.log("Its a vowels");
+}
+else if (a==="i"){
+    console.log("Its a vowelss");
+}else if (a==="o"){
+    console.log("Its a vowels");
+}else if (a==="u"){
+    console.log("Its a vowels");
+}else{
+    console.log("its a constonents");
+}
+
+let bavan=30;
+let suriya=40;
+let praveen=50;
+if(bavan>=praveen>=suriya){
+    console.log("bavan is gratest");
+}
+
+login
+let user="admin";
+let password="1234";
+if(user=="admin"){
+    console.log("welcome");
+}else{
+    console.log("user name is in-valid")
+}
+if(password=="1234"){
+    console.log("valid")
+}else{
+    console.log("password is in-valid")
+}
+
+how is gratest
+let a = 100;
+let b = 250;
+let c = 95;
+
+if (a>b && a>c){
+    console.log(a,"a is the Largest value");
+}
+else if (b>a && b>c){
+    console.log(b,"b is the greatest");
+}else{
+    console.log(c,"c is the graeatest");
+}
+odd or even
+let a = "7";
+if ("a/2==0"){
+    console.log("its a even number");
+}else if("a/2!==0") {
+    console.log("Its a odd number");
+}
